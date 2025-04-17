b5ea8c615603b53bfa09a34a61ca3eaf80dd6667
Meghna Chandra <meghnachandra@Meghnas-MacBook-Air-2.local>
2025-04-17 23:28:29 +0530

    updated homepage for small screens

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -54,10 +54,10 @@ const HomePage = () => {
 <hr />
 
 {/* Mobile version */}
-<div className="d-block d-md-none text-center mt-5 px-1"  style={{color: "gray"}}>
-  <p className="h4">Struggling to understand a LeetCode problem?</p>
+<div className="d-block d-md-none text-center mt-5 px-1"  style={{color: "rgb(220, 220, 220)"}}>
+  <p className="h4 py-2">Struggling to understand a LeetCode problem?</p>
   <p className="small">You've come to the right place.</p>
-  <p className="small">Get concise insights, real-world uses, and smart tips to solve problems—powered by AI.</p>
+  <p className="small">Gain clear insights, practical examples, and smart strategies to tackle problems, all with the help of AI.</p>
   <div className="text-center py-2">
     <Link
       to="/search"
@@ -91,19 +91,19 @@ const HomePage = () => {
   <img src="/images/problem-solving.png" alt="Inside VS Code" className="img-fluid" />
 </div>
     </div>
-    <div className="col-md-6 text-center">
+    <div className="col-md-6 d-flex justify-content-center px-3 px-md-5" style={{ flexDirection: 'column' }}>
       <h4 className="py-2" style={{ color: "#569cd6" }}>📌 Problem Breakdown</h4>
-      <p style={{ color: "#9cdcfe" }}>Understand what’s really being asked.</p>
-      <p style={{ color: "#ce9178" }}>Each LeetCode problem is broken down into simpler terms, eliminating any technical jargon. Whether it’s array manipulation or dynamic programming, the essence of the problem is explained in an easy-to-understand manner.</p>
+      <p className="px-3" style={{ color: "#9cdcfe" }}>Understand what’s really being asked.</p>
+      <p className="px-3" style={{ color: "#ce9178" }}>Each LeetCode problem is broken down into simpler terms, eliminating any technical jargon. Whether it’s array manipulation or dynamic programming, the essence of the problem is explained in an easy-to-understand manner.</p>
     </div>
   </div>
 
   {/* Section 2 */}
   <div className="row align-items-center my-5 py-2 flex-column-reverse flex-md-row">
-    <div className="col-md-6 text-center">
+    <div className="col-md-6 d-flex justify-content-center px-2 px-md-5" style={{ flexDirection: 'column' }}>
       <h4 className="py-2" style={{ color: "#569cd6" }}>🔧 Techniques & Algorithms</h4>
-      <p style={{ color: "#9cdcfe" }}>Know the tools for the job.</p>
-      <p style={{ color: "#ce9178" }}>Understand which algorithm or technique is best suited for each problem — whether it's two pointers, recursion, sliding window, or something more advanced. The explanation also covers why a particular technique is used, not just what it is.</p>
+      <p className="px-3" style={{ color: "#9cdcfe" }}>Know the tools for the job.</p>
+      <p className="px-3" style={{ color: "#ce9178" }}>Understand which algorithm or technique is best suited for each problem — whether it's two pointers, recursion, sliding window, or something more advanced. The explanation also covers why a particular technique is used, not just what it is.</p>
     </div>
     <div className="col-md-6 mb-3 mb-md-0">
     <div className="vscode-window border rounded-3 overflow-hidden mx-auto my-4" style={{ maxWidth: '700px' }}>
@@ -129,19 +129,19 @@ const HomePage = () => {
   <img src="/images/hacker-mindset.png" alt="Inside VS Code" className="img-fluid" />
 </div>
     </div>
-    <div className="col-md-6 text-center">
+    <div className="col-md-6 d-flex justify-content-center px-2 px-md-5" style={{ flexDirection: 'column' }}>
       <h4 className="py-2" style={{ color: "#569cd6" }}>🚀 Efficiency Tips</h4>
-      <p style={{ color: "#9cdcfe" }}>Write smarter, faster code.</p>
-      <p style={{ color: "#ce9178" }}>Get optimization tips to improve your code’s time and space complexity. Learn common pitfalls, how to avoid brute-force approaches, and how to move from a naive solution to an optimal one.</p>
+      <p className="px-3" style={{ color: "#9cdcfe" }}>Write smarter, faster code.</p>
+      <p className="px-3" style={{ color: "#ce9178" }}>Get optimization tips to improve your code’s time and space complexity. Learn common pitfalls, how to avoid brute-force approaches, and how to move from a naive solution to an optimal one.</p>
     </div>
   </div>
 
   {/* Section 4 */}
   <div className="row align-items-center my-5 py-2 flex-column-reverse flex-md-row">
-    <div className="col-md-6 text-center">
+    <div className="col-md-6 d-flex justify-content-center px-2 px-md-5" style={{ flexDirection: 'column' }}>
       <h4 className="py-2" style={{ color: "#569cd6" }}>🌍 Real-World Applications</h4>
-      <p style={{ color: "#9cdcfe" }}>Bridge theory and practice.</p>
-      <p style={{ color: "#ce9178" }}>Discover how similar problems are tackled in tech companies and real-world systems. From backend services to AI pipelines, understand the practical relevance of coding challenges in the industry.</p>
+      <p className="px-3" style={{ color: "#9cdcfe" }}>Bridge theory and practice.</p>
+      <p className="px-3" style={{ color: "#ce9178" }}>Discover how similar problems are tackled in tech companies and real-world systems. From backend services to AI pipelines, understand the practical relevance of coding challenges in the industry.</p>
     </div>
     <div className="col-md-6 mb-3 mb-md-0">
     <div className="vscode-window border rounded-3 overflow-hidden mx-auto my-4" style={{ maxWidth: '700px' }}>
